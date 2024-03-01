0a940a4c5d6ca560ed5a28091ad82af4791ede8b
Nishit Grover <grovernt@mail.uc.edu>
2024-03-01 18:17:58 -0500

    added .gitignore

--- api/index.js
+++ api/index.js
@@ -2,14 +2,19 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
+
 mongoose.connect(process.env.MONGO)
 .then( () => {
       console.log('Connected to database!');
    })
-   .catch( () => {
-      console.log('Connection failed!')});
+   .catch( (err) => {
+      console.log(err)});
 
-app.listen(3000, () => {
+const app = express();
+
+
+
+app.listen(1234, () => {
    console.log('Server is running on port 3000!!');
 });
