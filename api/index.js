12100d378dbe9e784febf3b0b6409642293f178f
Nishit Grover <grovernt@mail.uc.edu>
2024-03-02 21:22:51 -0500

    test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import UserRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -18,3 +19,7 @@ const app = express();
 app.listen(1234, () => {
    console.log('Server is running on port 3000!!');
 });
+
+
+app.use('/api/user', UserRoutes);
+
